fcba527ccc1d9d6ef9069bff0df811476012ffc5
Aryan <bhardwaj7a@gmail.com>
2025-03-03 14:47:28 +0530

    Add Broadcast Function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // Creating a websocket server
-import { WebSocketServer } from "ws";
-
+import { WebSocketServer , WebSocket } from "ws";
 const wss = new WebSocketServer({port : 8080}); 
-// Above two lines create a new server
+let allSockets:WebSocket[] = [];
 
 let userCount = 0;
 // socket is used for all the communication with the client 
 wss.on("connection" , (socket) => {
+    allSockets.push(socket);
     userCount = userCount + 1 ;
     console.log("User Connected #" + userCount)
 
@@ -14,6 +14,13 @@ wss.on("connection" , (socket) => {
     socket.on("message" , (message) => {
         console.log("message received " + message.toString())
         // Whatever message , we are receiving from the client , we are sending them back the message as it is.
-        socket.send(message.toString() + ": sent from the server")
+
+        // Broadcast the message to all clients / sockets 
+        for(let i=0; i < allSockets.length; i++)
+        {
+            const s = allSockets[i];
+            s.send(message.toString() + ": sent from the server")
+        }
+        
     })
 })
